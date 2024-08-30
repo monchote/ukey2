d11ddc4c0ef4b61225e21c2095af81c332269996
Ramon Arguello <monchote@gmail.com>
2024-08-30 23:40:17 +0200

    Update crypto_ops_openssl.cc

--- src/securemessage/src/securemessage/crypto_ops_openssl.cc
+++ src/securemessage/src/securemessage/crypto_ops_openssl.cc
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-#include <openssl/aes.h>
-#include <openssl/bn.h>
-#include <openssl/ec.h>
-#include <openssl/ecdsa.h>
-#include <openssl/evp.h>
-#include <openssl/hmac.h>
-#include <openssl/obj_mac.h>
-#include <openssl/ossl_typ.h>
-#include <openssl/rand.h>
-#include <openssl/rsa.h>
-#include <openssl/sha.h>
-#include <openssl/x509.h>
+#include "openssl/aes.h"
+#include "openssl/bn.h"
+#include "openssl/ec.h"
+#include "openssl/ecdsa.h"
+#include "openssl/evp.h"
+#include "openssl/hmac.h"
+#include "openssl/obj_mac.h"
+#include "openssl/ossl_typ.h"
+#include "openssl/rand.h"
+#include "openssl/rsa.h"
+#include "openssl/sha.h"
+#include "openssl/x509.h"
 
 #include <stddef.h>
 #include <climits>
